3e9c1ac8bcc177ebb1c123602ea93bac0c884db8
Jason Hochheiser <jsunh98@gmail.com>
2019-03-31 21:02:59 -0400

    fixed bugs

--- team-manager-front-end/src/components/divisions/AddDivision.js
+++ team-manager-front-end/src/components/divisions/AddDivision.js
@@ -4,7 +4,7 @@ export class AddDivision extends Component {
 
 state = {
   divisionName : "",
-  conference : "",
+
 
 }
 
@@ -13,7 +13,7 @@ state = {
         event.preventDefault()
         this.props.addDivision(this.props.conference.conferenceId, this.state)
         console.log(this.props.conference)
-        this.setState({ divisionName : "", conference :"" })
+        this.setState({ divisionName : "" })
     }
 
     onChange = (event) => this.setState({ [event.target.name]: event.target.value })
@@ -22,7 +22,7 @@ state = {
       return (
         <form onSubmit={this.onSubmit} style={{display: 'flex'}}>
           <input type="text" name="divisionName" placeholder="division name" value={this.state.divisionName} onChange={this.onChange} style={{flex: '10', padding: '5px'}} />
-  
+
 
           <input type="submit" value="Submit" className="btn" style={{flex: '1'}} />
         </form>
